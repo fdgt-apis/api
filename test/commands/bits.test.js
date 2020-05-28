b3f37305fdf3d63c89f1d9cc8756192982ba2606
Trezy <tre@trezy.com>
2020-05-28 11:01:57 -0500

    test(event/bits): add tests

---
+++ test/commands/bits.test.js
@@ -0,0 +1,104 @@
+// Module imports
+const {
+	spy,
+	useFakeTimers,
+} = require('sinon')
+const { expect } = require('chai')
+const { parse: parseIRCMessage } = require('irc-message')
+const { v4: uuid } = require('uuid')
+const EventEmitter = require('events')
+const faker = require('faker')
+
+
+
+
+
+// Local imports
+const CAPABILITIES = require('../../data/CAPABILITIES')
+const Channel = require('../../structures/Channel')
+const Connection = require('../../structures/Connection')
+const User = require('../../structures/User')
+
+
+
+
+
+// Local constants
+const testChannelName = 'TestChannel'
+const testOauthToken = 'oauth:1234567890'
+const testUsername = 'Bob'
+const ircSocket = class extends EventEmitter {
+	end = () => {}
+	write = () => {}
+}
+const wsSocket = class extends EventEmitter {
+	send = () => {}
+	terminate = () => {}
+}
+
+
+
+
+
+describe.only('bits events', function() {
+	const clock = useFakeTimers()
+	const fdgtUser = new User({ username: 'fdgt' })
+
+	let connection = null
+	let socket = null
+
+	beforeEach(() => {
+		socket = new wsSocket
+		connection = new Connection({
+			fdgtUser,
+			socket,
+		})
+
+		socket.emit('message', `NICK ${testUsername}`)
+		socket.emit('message', `PASS ${testOauthToken}`)
+		socket.emit('message', `CAP REQ ${CAPABILITIES.join(' ')}`)
+		socket.emit('message', 'CAP END')
+		socket.emit('message', `JOIN #${testChannelName}`)
+
+		spy(connection)
+		spy(socket)
+	})
+
+	afterEach(() => {
+		connection.close()
+		connection = null
+		socket = null
+	})
+
+	it('should simulate a `bits` event', () => {
+		socket.emit('message', `PRIVMSG #${testChannelName} bits`)
+
+		const rawMessage = connection.send.getCall(0).firstArg
+		const { tags } = parseIRCMessage(rawMessage)
+
+		expect(tags.bits).to.exist
+	})
+
+	it('should forward the message', () => {
+		const message = faker.lorem.sentence()
+		socket.emit('message', `PRIVMSG #${testChannelName} bits ${message}`)
+
+		const rawMessage = connection.send.getCall(0).firstArg
+		const {
+			params: [, forwardedMessage],
+			tags,
+		} = parseIRCMessage(rawMessage)
+
+		console.log(forwardedMessage)
+		expect(forwardedMessage).to.equal(message)
+	})
+
+	it('should send the correct amount of bits', () => {
+		socket.emit('message', `PRIVMSG #${testChannelName} bits --bitscount 99999`)
+
+		const rawMessage = connection.send.getCall(0).firstArg
+		const { tags } = parseIRCMessage(rawMessage)
+
+		expect(tags.bits).to.equal('99999')
+	})
+})
