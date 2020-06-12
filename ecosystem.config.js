44249ef9fb2e17017f1d8c795f7a0ab8152cf662
Trezy <tre@trezy.com>
2020-06-12 16:23:17 -0500

    chore: update `master` to `main`

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
 			host: [process.env.DEPLOY_HOST],
 			path: '/var/www/fdgt',
 			'post-deploy': 'source ~/.zshrc; yarn install; yarn build;',
-			ref: 'origin/master',
+			ref: 'origin/main',
 			repo,
 			user: 'deploy',
 		},
