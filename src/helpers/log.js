5e5e2adeaf9f281725f5cc60f271a7434ed407ae
Trezy <tre@trezy.com>
2020-08-03 21:35:20 -0500

    refactor(log): only push logs to Firebase on prod

--- src/helpers/log.js
+++ src/helpers/log.js
@@ -27,7 +27,7 @@ const logger = Logger.createLoggerFromName('@fdgt/api')
 
 
 module.exports = (message, meta = {}, type = 'log') => {
-	if (NODE_ENV !== 'test') {
+	if (NODE_ENV === 'production') {
 		firestore.collection('logs').add({
 			createdAt: firebaseAdmin.firestore.Timestamp.now(),
 			message,
