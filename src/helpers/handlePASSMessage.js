37f104875b5d04165b2616a3fd07b8e0d2cc4758
Trezy <tre@trezy.com>
2020-08-15 11:08:37 -0500

    feat(handlePASSMessage): create a new `appConnection` if the token contains the ID of an app

--- src/helpers/handlePASSMessage.js
+++ src/helpers/handlePASSMessage.js
@@ -1,5 +1,26 @@
-export default (message, connection) => {
+// Local imports
+import {
+	firebaseAdmin,
+	firestore,
+} from 'helpers/firebase'
+
+
+
+
+
+export default async (message, connection) => {
 	const [token] = message.params
+	const appID = token.replace(/^oauth:/, '')
+
+	const app = (await firestore.collection('apps').doc(appID).get()).data()
+
+	if (app) {
+		firestore.collection('appConnections').add({
+			appID,
+			connectionID: connection.id,
+		})
+	}
+
 	connection.token = token
 	connection.emit('acknowledge')
 }
