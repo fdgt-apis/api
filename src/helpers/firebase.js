71d679ea52b8f28e7f16145a5f58f499868a221f
Trezy <tre@trezy.com>
2020-08-18 10:48:45 -0500

    style(firebase): update destructuring

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -8,7 +8,20 @@ import * as firebaseAdmin from 'firebase-admin'
 
 
 // Local constants
-const { NODE_ENV } = process.env
+const {
+	FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+	FIREBASE_AUTH_URI,
+	FIREBASE_CLIENT_EMAIL,
+	FIREBASE_CLIENT_ID,
+	FIREBASE_CLIENT_X509_CERT_URL,
+	FIREBASE_PRIVATE_KEY_ID,
+	FIREBASE_PRIVATE_KEY,
+	FIREBASE_PROJECT_ID,
+	FIREBASE_TOKEN_URI,
+	FIREBASE_TYPE,
+	FIREBASE_DATABASE_URL,
+	NODE_ENV,
+} = process.env
 
 
 
@@ -24,18 +37,18 @@ let app = null
 if (NODE_ENV !== 'test') {
 	app = firebaseAdmin.apps[0] || firebaseAdmin.initializeApp({
 		credential: firebaseAdmin.credential.cert({
-			auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-			auth_uri: process.env.FIREBASE_AUTH_URI,
-			client_email: process.env.FIREBASE_CLIENT_EMAIL,
-			client_id: process.env.FIREBASE_CLIENT_ID,
-			client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-			private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-			private_key: process.env.FIREBASE_PRIVATE_KEY,
-			project_id: process.env.FIREBASE_PROJECT_ID,
-			token_uri: process.env.FIREBASE_TOKEN_URI,
-			type: process.env.FIREBASE_TYPE,
+			auth_provider_x509_cert_url: FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+			auth_uri: FIREBASE_AUTH_URI,
+			client_email: FIREBASE_CLIENT_EMAIL,
+			client_id: FIREBASE_CLIENT_ID,
+			client_x509_cert_url: FIREBASE_CLIENT_X509_CERT_URL,
+			private_key_id: FIREBASE_PRIVATE_KEY_ID,
+			private_key: FIREBASE_PRIVATE_KEY,
+			project_id: FIREBASE_PROJECT_ID,
+			token_uri: FIREBASE_TOKEN_URI,
+			type: FIREBASE_TYPE,
 		}),
-		databaseURL: process.env.FIREBASE_DATABASE_URL,
+		databaseURL: FIREBASE_DATABASE_URL,
 	})
 }
 
